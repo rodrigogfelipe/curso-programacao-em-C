cb5eb773b40f9d5ea19419981da7ecf090b61969
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2020-01-17 16:36:27 -0300

    Aula 10 Variaveis

---
+++ programas1.c
@@ -0,0 +1,14 @@
+/*Aula sobre variaveis*/
+#include <stdio.h>
+
+int main(){
+
+	int idade;
+
+	printf("Qual e a sua idade?\n");
+	/*Receber dados da entrada padrao*/
+	scanf("%d", &idade);
+	printf("A sua idade é %d", idade);
+
+	return 0;
+}
