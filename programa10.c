e4a150c02c2695c7bcbbcace16b2e4ddd71383a2
Rodrigo Felipe <rodrigogfelipe@gmail.com>
2020-02-19 13:38:38 -0300

    Tipos Booleanos

---
+++ programa10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*booleanos a linguagem C reconhece o valor zero como falso e outro valor diferente verdadeiro*/
+int main(){
+	int booleano = 0;
+	if(booleano) {
+		printf("Verdadeiro");
+	} else {
+		printf("Falso");
+	}
+
+
+	return  0;
+}
